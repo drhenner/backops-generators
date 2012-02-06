f598fe1a3cb080665ce6386b6a86bd6c1fd5a6fe
David Henner <drhenner@yahoo.com>
2012-02-06 17:14:19 -0500

    generator improvements

--- lib/generators/nifty/scaffold/templates/actions/index.rb
+++ lib/generators/nifty/scaffold/templates/actions/index.rb
@@ -1,5 +1,6 @@
   def index
     params[:page] ||= 1
     params[:rows] ||= 20
-    @<%= instances_name %> = <%= class_name %>.paginate(:page => params[:page].to_i, :per_page => params[:rows].to_i)
+    @<%= instances_name %> = <%= class_name %>.order(sort_column + " " + sort_direction).
+                                              paginate(:page => params[:page].to_i, :per_page => params[:rows].to_i)
   end
