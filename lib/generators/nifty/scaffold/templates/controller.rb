235e0f9d81af11b5f7bf7e726252b218d08f6c04
drhenner <drhenner@yahoo.com>
2014-02-25 15:11:33 -0800

    show schema by default

--- lib/generators/nifty/scaffold/templates/controller.rb
+++ lib/generators/nifty/scaffold/templates/controller.rb
@@ -1,14 +1,11 @@
 class <%= plural_class_name %>Controller < ApplicationController
-  helper_method :sort_column, :sort_direction
+  helper_method :sort_column
   <%= controller_methods :actions %>
 
   private
 
-    def sort_column
-      <%= class_name %>.column_names.include?(params[:sort]) ? params[:sort] : '<%= model_attributes.first.name %>'
-    end
+  def sort_column
+    <%= class_name %>.column_names.include?(params[:sort]) ? params[:sort] : '<%= model_attributes.first.name %>'
+  end
 
-    def sort_direction
-      %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
-    end
 end
--- lib/generators/nifty/scaffold/templates/model.rb
+++ lib/generators/nifty/scaffold/templates/model.rb
@@ -1,4 +1,7 @@
+# Schema
+<%- model_attributes.each do |a| -%>
+<%= "# #{a.name}" %>
+<%- end -%>
 class <%= class_name %> < ActiveRecord::Base
 <%= "  set_table_name :#{table_name}\n" if table_name -%>
-  attr_accessible <%= model_attributes.map { |a| ":#{a.name}" }.join(", ") %>
 end
